6ec900cc6b5549fc2f04cb056e8971d4550f0a97
Howard Jing <howard.jing@gmail.com>
2011-11-29 11:05:35 -0500

    abstracted out prob1

--- matlab/hw4/prob1.m
+++ matlab/hw4/prob1.m
@@ -1,55 +1,57 @@
-function [appxderiv deriv] = prob1
+function [max_error] = prob1
  
     xj = setUpGrid();
-    [leny lenx] = size(xj);
+    [rowMax colMax] = size(xj);
         
     fxj = zeros(size(xj));
-    derivativexj = zeros(size(xj));
+    derivative_xj = zeros(size(xj));
     appx_deriv_xj = zeros(size(xj));
     
     % f(x)
-    sin50 = @(x) sin(50*x);
+    sin50 = @(x) sin(50.*x);
     % f'(x)
-    derivative = @(x) 50*cos(50*x);
-    % multiplier is 2*pi*i/period
-    multiplier = 50*1i;
-    for i = 1:leny
-        for k = 1:lenx
-            fxj(i,k) = sin50(xj(i,k));
-            if k <= 2^(3+k) 
-                derivativexj(i,k) = derivative(xj(i,k));
-            end
-        end
-        appx_deriv_xj(i,:) = approx(fxj(i,:),multiplier);
-    end
-    error = abs(appx_deriv_xj - derivativexj);
-    max_error = zeros(1,leny);
-    for i = 1:leny
-        max_error(i) = max(error(i,:));
+    derivative = @(x) 50*cos(50.*x);
+    % period of f(x)
+    period = 2*pi/50;
+
+    % do the various calculations
+    for i = 1:rowMax
+        len = 2^(i+3);
+        fxj(i,1:len) = sin50(xj(i,1:len));
+        derivative_xj(i,1:len) = derivative(xj(i,1:len));
+        appx_deriv_xj(i,1:len) = approx(fxj(i,1:len), period);
     end
     
-    appxderiv = appx_deriv_xj(1,:);
-    deriv = derivativexj(1,:);
+    % find errors
+    error = abs(appx_deriv_xj - derivative_xj);
+    max_error = zeros(1,rowMax);
+    
+    for i = 1:rowMax
+        len = 2^(i+3);
+        max_error(i) = max(error(i,1:len));
+    end
+    
+    appxderiv = appx_deriv_xj(1,1:16);
+    deriv = derivative_xj(1,1:16);
 end
 
 % find an approximation for the derivative of a function using fft
-function [approximation] = approx(fx, multiplier)
+function [approximation] = approx(fx, period)
+    multiplier = 2*pi*1i/period;
     transform = fft(fx);
-    alpha = 1:length(fx);
+    alpha = 0:(length(fx)-1);
     transformed_deriv = multiplier.*alpha.*transform;
     approximation = ifft(transformed_deriv);
 end
 
 % returns the properly set up uniform grid
 function [xj] = setUpGrid
-    % setup the problem
+    % sets up the problem
     temp = 4:10;
     N = 2.^temp;
     j = zeros(7,N(length(N)));
     for i = 1:length(N)
-        for k = 1:N(i)
-            j(i,k) = k-1;
-        end
+        j(i,1:N(i)) = 0:N(i)-1;
     end
 
     h = zeros(1,length(N));
@@ -57,8 +59,6 @@ function [xj] = setUpGrid
 
     xj = zeros(size(j));
     for i = 1:length(N)
-        for k = 1:length(j)
-            xj(i,k) = j(i,k)*h(i);
-        end
+        xj(i,1:N(i)) = j(i,1:N(i))*h(i);
     end
 end
