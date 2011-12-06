3a9020cdb335be8a0fdc17fd272bc867c878b14e
Howard Jing <howard.jing@gmail.com>
2011-12-06 14:35:52 -0500

    fixed bug, rk4 works now

--- matlab/hw5/lorenzTester.m
+++ matlab/hw5/lorenzTester.m
@@ -1,7 +1,7 @@
 function [ft] = lorenzTester
     h = 0.01;
     initialPoints = [0.1 -0.1 0.0];
-    t = 1:0.1:100;
+    t = 0:h:100;
     
     ft = zeros(length(t),3);
     
@@ -15,29 +15,25 @@ end
 function [answer] = rk4Step(x,h)
     % y_n+1 = y_n + 1/6 (e_1 + 2e_2 + 2e_3 + e_4);
     
-    xi_1 = xi(x,h);
-    xi_2 = xi(x+0.5*xi_1, 0.5*h);
-    xi_3 = xi(x+0.5*xi_2, 0.5*h);
-    xi_4 = xi(x+xi_3, h);
+    xi_1 = h*lorenzEquations(x);
+    xi_2 = h*lorenzEquations(x+xi_1/2);
+    xi_3 = h*lorenzEquations(x+xi_2/2);
+    xi_4 = h*lorenzEquations(x+xi_3);
     
-    answer = x + (1/6)*(xi_1 + 2*xi_2 + 2*xi_3 + xi_4);
+    answer = x + (xi_1 + 2*xi_2 + 2*xi_3 + xi_4)/6;
 end
 
-function [answer] = xi(x,h)
-    answer = h*lorenzEquations(x);
-end
-
-% takes (x1, x2, x3) and t and parameters
+% takes (x1, x2, x3) as parameters
 % outputs the derivative
-function [xPrime yPrime zPrime] = lorenzEquations(x)
+function [deriv] = lorenzEquations(x)
 
     % Lorenz Equations
     sigma = 10;
     beta = 8/3;
     rho = 28;
-
-    xPrime = (sigma*(x(2) - x(1)));
-    yPrime = (rho*x(1) - x(2) - x(1)*x(3));
-    zPrime = (-beta*x(3) + x(1)*x(2));
     
+    xPrime = -sigma*x(1) + sigma*x(2);
+    yPrime = rho*x(1) - x(2) - x(1)*x(3);
+    zPrime = x(1)*x(2)-beta*x(3);
+    deriv = [xPrime yPrime zPrime];    
 end
